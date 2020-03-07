2e0779bcef3288c8f2500c461686b90911dc5149
Gander-Alexander <Gander-Alexander@users.noreply.github.com>
2020-03-07 01:25:19 -0700

    added the images for assignment 2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
 const pageInfo = require('./pageInfo');
+const gallery = require('./gallery')
 const moment = require('moment');
 
 
